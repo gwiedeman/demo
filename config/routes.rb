02a4d3ccd84da3fdaa23bc341fa274b3b465eb97
gwiedeman <gregory.wiedeman1@gmail.com>
2022-12-08 11:51:56 -0500

    setup subpath route for prod

--- config/routes.rb
+++ config/routes.rb
@@ -1,30 +1,34 @@
 Rails.application.routes.draw do
 
-  concern :range_searchable, BlacklightRangeLimit::Routes::RangeSearchable.new
-  mount Blacklight::Engine => '/'
-    mount Arclight::Engine => '/'
+  scope 'demo' do
 
-  root to: "catalog#index"
-  concern :searchable, Blacklight::Routes::Searchable.new
+    concern :range_searchable, BlacklightRangeLimit::Routes::RangeSearchable.new
+    mount Blacklight::Engine => '/'
+      mount Arclight::Engine => '/'
 
-  resource :catalog, only: [:index], as: 'catalog', path: '/catalog', controller: 'catalog' do
-    concerns :searchable
-    concerns :range_searchable
+    root to: "catalog#index"
+    concern :searchable, Blacklight::Routes::Searchable.new
 
-  end
-  devise_for :users
-  concern :exportable, Blacklight::Routes::Exportable.new
+    resource :catalog, only: [:index], as: 'catalog', path: '/catalog', controller: 'catalog' do
+      concerns :searchable
+      concerns :range_searchable
 
-  resources :solr_documents, only: [:show], path: '/catalog', controller: 'catalog' do
-    concerns :exportable
-  end
-
-  resources :bookmarks do
-    concerns :exportable
-
-    collection do
-      delete 'clear'
     end
+    devise_for :users
+    concern :exportable, Blacklight::Routes::Exportable.new
+
+    resources :solr_documents, only: [:show], path: '/catalog', controller: 'catalog' do
+      concerns :exportable
+    end
+
+    resources :bookmarks do
+      concerns :exportable
+
+      collection do
+        delete 'clear'
+      end
+    end
+  
   end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
