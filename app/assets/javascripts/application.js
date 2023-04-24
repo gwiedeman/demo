a5d7dd3be0eb06145a6fca349b168b28c38d0faf
gwiedeman <gregory.wiedeman1@gmail.com>
2023-04-24 12:42:15 -0400

    switched to ruby 2.7.7

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,3 +12,16 @@
 //= require arclight/arclight
 //= require stickyfill
 
+// Matomo
+  var _paq = window._paq = window._paq || [];
+  /* tracker methods like "setCustomDimension" should be called before "trackPageView" */
+  _paq.push(['trackPageView']);
+  _paq.push(['enableLinkTracking']);
+  (function() {
+	var u="https://library-albany.matomo.cloud/";
+	_paq.push(['setTrackerUrl', u+'matomo.php']);
+	_paq.push(['setSiteId', '2']);
+	var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
+	g.async=true; g.src='//cdn.matomo.cloud/library-albany.matomo.cloud/matomo.js'; s.parentNode.insertBefore(g,s);
+  })();
+// End Matomo Code
